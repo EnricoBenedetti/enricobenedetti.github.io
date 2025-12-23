277150b5e28c94e57146c56ac69d863181ff0e79
Enri <98enri98@gmail.com>
2025-12-23 15:08:03 +0100

    up

--- assets/js/hydra-bg.js
+++ assets/js/hydra-bg.js
@@ -2,10 +2,10 @@
 var hydra = new Hydra({
   canvas: document.getElementById("myCanvas"),
   detectAudio: false
-})
+});
 
 //hydra.loadScript("https://cdn.statically.io/gl/metagrowing/extra-shaders-for-hydra/main/lib/lib-screen.js").then(xp_background)
-hydra.loadScript("/assets/js/lib-screen.js").then(xp_background)
+hydra.loadScript("/assets/js/lib-screen.js").then(xp_background);
 
 function animation() {
   //s0.initImage("/static/assets/DSC00079_lowq.png")
@@ -29,7 +29,7 @@ function xp_background() {
   s0.initImage("https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png")
   src(s0).contrast(2.3).saturate().dither4().out(o0)
 
-  osc(900,0.1).thresh(0.9999,0.00001).modulate(noise(0.3)).out(o2)
+  osc(900, 0.1).thresh(0.9999, 0.00001).modulate(noise(0.3)).out(o2)
 
   src(o0).modulate(src(o2), 0.001).out(o1)
   render(o1)
